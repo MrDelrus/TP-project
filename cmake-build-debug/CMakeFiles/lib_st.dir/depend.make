b298d603ef84224584f9dd85149771bf312ca3ea
Blinov-Ilya <blinov.is@phystech.edu>
2022-04-11 00:14:07 +0300

    Big changes in all

---
+++ cmake-build-debug/CMakeFiles/lib_st.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for lib_st.
+# This may be replaced when dependencies are built.
---
+++ main.cpp
@@ -0,0 +1,44 @@
+#pragma once
+#include <string>
+#include <set>
+#include <iostream>
+
+//class Person {
+//private:
+//    std::string name;
+//public:
+//
+//    std::string GetName() {
+//        return name;
+//    }
+//
+//    bool operator < (const Person& person) const {
+//        return (name < person.name);
+//    }
+//};
+//
+//class Tutor : public Person {
+//private:
+//    std::string name;
+//public:
+//    explicit Tutor(std::string  name): name(std::move(name)) {}
+//    Tutor(const Tutor&) = default;
+//    ~Tutor() = default;
+//};
+
+struct X
+{
+    enum direction { left = 'l', right = 'r' };
+};
+
+int main() {
+    X x;
+    X* p = &x;
+
+    int a = X::direction::left; // allowed only in C++11 and later
+    int b = X::left;
+    int c = x.left;
+    int d = p->left;
+
+    return 0;
+}

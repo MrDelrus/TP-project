7f892731d12b8ccefe3413acaf18f93b95f7f780
Blinov-Ilya <blinov.is@phystech.edu>
2022-04-14 18:15:30 +0300

    Adding CMake, builds

---
+++ build/CMakeFiles/chat_lib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chat_lib.dir/Chat.cpp.o"
+  "libchat_lib.pdb"
+  "libchat_lib.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chat_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

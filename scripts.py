51c3ba7d766e35e723b7776c0529a3757baad2bf
Blinov-Ilya <blinov.is@phystech.edu>
2022-05-06 11:36:34 +0300

    Deleting extra files and some renaming

--- scripts.py
+++ scripts.py
@@ -1,42 +1,67 @@
+import socket
+
+symbol = '#'
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = 'something'
+port = 1984
+s.bind((host, port))
+
+def GET_ANSWER(request):
+    l = len(request)
+    request = f'{l:03}' + request
+    s.send(request)
+    answer = s.recv(1024)
+    return answer
+
 def SIGN_UP(type, login, password):
-    return True
-# True, если получилось зарегистрироваться (= не существует пользователя с таким же именем), False иначе
+    request = symbol + 'SIGN_UP' + symbol + type + symbol + login + symbol + password
+    return GET_ANSWER(request)
 
 def SIGN_IN(login, password):
-    return True
-# Вызвать CHECK_PASSWORD на сервере
+    request = symbol + 'SIGN_IN' + symbol + login + symbol + password
+    return GET_ANSWER(request)
 
 def IS_TUTOR(login):
-    return True
-# Проверка, что человек - Tutor
+    request = symbol + 'IS_TUTOR' + symbol + login
+    return GET_ANSWER(request)
 
 def GET_GROUPS(login):
-    return 'Информатика\nМатематика\nКлоуны'
-# Вернуть str с именами групп через '\n'
+    request = symbol + 'GET_GROUPS' + symbol + login
+    return GET_ANSWER(request)
 
 def ADD_GROUP(login, name):
-    pass
+    request = symbol + 'ADD_GROUP' + symbol + login + symbol + name
+    return GET_ANSWER(request)
 
 def ENTER_GROUP(group_name):
-    pass
+    request = symbol + 'ENTER_GROUP' + symbol + group_name
+    return GET_ANSWER(request)
 
 def GET_STUDENTS_IN_GROUP(login, group_name):
-    return 'Алиса\nБоб'
+    request = symbol + 'GET_STUDENT_IN_GROUP' + symbol + login + symbol + group_name
+    return GET_ANSWER(request)
 
 def GET_TASKS(login, group_name):
-    return 'А\nВ\nС'
+    request = symbol + 'GET_TASKS' + symbol + login + symbol + group_name
+    return GET_ANSWER(request)
 
 def ADD_TASK(login, group_name, task_name, student_name):
-    pass
+    request = symbol + 'ADD_TASK' + symbol + login + symbol + group_name + symbol + task_name + symbol + student_name
+    return GET_ANSWER(request)
 
 def ADD_STUDENT_IN_GROUP(login, group_name, student_name):
-    pass
+    request = symbol + 'SIGN_IN' + symbol + login + symbol + group_name + symbol + student_name
+    return GET_ANSWER(request)
 
 def GET_TASK_TEXT(login, group_name, task_name):
-    return 'Решите в целых числах:\nx^n+y^n=z^n'
+    request = symbol + 'SIGN_IN' + symbol + login + symbol + group_name + symbol + task_name
+    return GET_ANSWER(request)
 
 def GET_LAST_5_MESSAGES(login, group_name, task_name):
-    return 'Q\nQQ\nQQQ\nQQQQ\nQqQQQQ'
+    request = symbol + 'SIGN_IN' + symbol + login + symbol + group_name + symbol + task_name
+    return GET_ANSWER(request)
 
 def SEND_MESSAGE(login, group_name, task_name, message):
-    pass
+    request = symbol + 'SIGN_IN' + symbol + login + symbol + group_name + symbol + task_name + symbol + message
+    return GET_ANSWER(request)
